8b1dca39c72eb22d8ed1e2fb364731699715b617
abhigya <abhigyadufare>
2025-02-06 21:09:07 -0500

    added functionality to update state and submit form

--- src/Components/FeedbackForm.jsx
+++ src/Components/FeedbackForm.jsx
@@ -7,17 +7,62 @@ const FeedbackForm = () => {
         email: '',
         feedback: ''
     });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const confirmationMessage = `
+            Name: ${formData.name} 
+            Email: ${formData.email} 
+            Feedback: ${formData.feedback}
+        `;
+        const isConfirmed = window.confirm(`Please confirm your details: \n\n${confirmationMessage}`);
+        if (isConfirmed) {
+            console.log('Submitting feedback: ', formData);
+            setFormData({
+                name: '',
+                email: '',
+                feedback: ''
+            });
+            alert('Thank you for your valuable feedback!');
+        };
+    }
+
   return (
     <>
     <nav>
     Tell Us What You Think
     </nav>
-      <form className="feedback-form">
+      <form onSubmit={handleSubmit} className="feedback-form">
         <h2>We'd Love to Hear From You!</h2>
         <p>Please share your feedback with us.</p>
-        <input type='text' name="name" placeholder='Your Name' />
-        <input type='email' name='email' placeholder='Your Email' />
-        <textarea name='feedback' placeholder='Your Feedback'></textarea>
+        <input 
+            type='text' 
+            name="name" 
+            placeholder='Your Name' 
+            value={formData.name}
+            onChange={handleChange}
+        />
+        <input 
+            type='email' 
+            name='email' 
+            placeholder='Your Email' 
+            value={formData.email} 
+            onChange={handleChange}
+        />
+        <textarea 
+            name='feedback' 
+            placeholder='Your Feedback' 
+            value={formData.feedback} 
+            onChange={handleChange}
+        ></textarea>
         <button type='submit'>Submit Feedback</button>
       </form>
     </>
